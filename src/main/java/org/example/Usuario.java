7cede74746ac0a9afbb017b6ae0ef3f46dd34b7e
iNxTSam <inxtsam@gmail.com>
2026-08-31 12:27:14 -0500

    Constructores sobrecargados, getters y setters toString junto a @Override

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -14,7 +14,57 @@ public class Usuario {
         this.telefono = telefono;
         this.activo = activo;
 
+    }
 
+    public Usuario(String nombreUsuario, int id, long telefono){
+
+        this(nombreUsuario, id, telefono, true);
+
+    }
+
+    public  Usuario(String nombreUsuario, int id){
+
+        this(nombreUsuario, id, 0L, true);
+
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+
+    public String toString(){
+        return "Usuario [nombre=" + nombreUsuario + ", id=" + id
+                + ", telefono=" + telefono + ", activo=" + activo + "]";
     }
 
 }
