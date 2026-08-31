c8bf7ff757adad32f7d78edd593e089d207b863c
iNxTSam <inxtsam@gmail.com>
2026-08-31 12:53:39 -0500

    Constructores sobrecarcados, junto a getters y setters, mas @Override del toString padre

--- src/main/java/org/example/Trabajador.java
+++ src/main/java/org/example/Trabajador.java
@@ -23,4 +23,49 @@ public class Trabajador extends Usuario{
         this.salario = salario;
     }
 
+    public Trabajador(String nombreUsuario, int id, char cargo){
+        super(nombreUsuario,id);
+        this.cargo = cargo;
+        this.horasSemanales = 0;
+        this.salario = 0.0;
+        this.contrasena = "default";
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public char getCargo() {
+        return cargo;
+    }
+
+    public int getHorasSemanales() {
+        return horasSemanales;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setCargo(char cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setHorasSemanales(int horasSemanales) {
+        this.horasSemanales = horasSemanales;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString(){
+        return "Usuario [nombre=" + nombreUsuario + ", id=" + id
+                + ", cargo=" + cargo + ", horasSemanales=" + horasSemanales + ", salario=" + salario + "]";
+    }
 }
