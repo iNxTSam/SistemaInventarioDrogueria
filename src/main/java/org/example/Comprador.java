b6d4d20e254ba8c2a719e2d048b3dadc5303a6cb
iNxTSam <inxtsam@gmail.com>
2026-09-01 14:56:11 -0500

    Clase comprador creada, atributos y constructor inicial creados

---
+++ src/main/java/org/example/Comprador.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Comprador extends Usuario{
+
+    private String contrasena;
+    private char metodoPago;
+    private int comprasRealizadas;
+    private double presupuesto;
+
+    public Comprador(String nombreUsuario, int id, long telefono, boolean activo, String contrasena, char metodoPago, int comprasRealizadas, double presupuesto){
+        super(nombreUsuario,id,telefono,activo);
+        this.contrasena = contrasena;
+        this.metodoPago = metodoPago;
+        this.comprasRealizadas = comprasRealizadas;
+        this.presupuesto = presupuesto;
+
+    }
+
+
+
+}
